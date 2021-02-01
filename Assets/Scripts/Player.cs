922824dd415c743905796b569f1b5a0b26540b8c
Justin Roma <jlroma.cxv@gmail.com>
2021-02-01 16:37:11 +0800

    Inital import of Unity project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float jumpInput;
+    private Rigidbody rigidbodyComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        jumpInput = Input.GetAxis("Jump");
+    }
+
+    // FixedUpdate is called once every physics update
+    private void FixedUpdate() {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, 0, verticalInput);
+    }
+}
