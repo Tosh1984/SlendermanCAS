a47ac7efb594f05bb8fa96cda5895babfb511d0d
Justin Roma <jlroma.cxv@gmail.com>
2021-02-04 20:25:40 +0800

    Added slenderman assets and reticle pointer
    AKA "copycat" update since the changes are simply imported free assets.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Managers;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+    [SerializeField] private float collectTime = 3f;
+
+    private Transform player;
+    private const float CollectingDistance = 0.1f;
+
+    private void Start() {
+        player = GameManager.Instance.GetPlayer();
+    }
+
+    public void Collect() {
+        Collider collider = GetComponent<Collider>();
+        if (collider) collider.enabled = false;
+
+        StartCoroutine(Collect(collectTime));
+    }
+
+    private IEnumerator Collect(float delay) {
+        float time = 0;
+
+        while (time < delay && Vector3.Distance(transform.position, player.position) > CollectingDistance) {
+            transform.position = Vector3.Lerp(transform.position, player.position, time / delay);
+            yield return null;
+            time += Time.deltaTime;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/VisualEffects/Highlight.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: rename Highlight to a more accurate name
+namespace Animations {
+    public class Highlight : MonoBehaviour {
+
+        [SerializeField] private float transitionTime = 1f;
+
+        private Vector3 originalScale;
+        private Coroutine transition;
+
+        private void Awake() {
+            originalScale = transform.localScale;
+        }
+
+        public void Scale(float scale) {
+            StartTransition(scale);
+        }
+
+        public void ScaleAndReset(float scale) {
+            float currentScale = transform.localScale.x / originalScale.x;
+
+            StartTransition(new List<float> { scale, currentScale });
+        }
+
+        public void ResetScale() {
+            StartTransition(1);
+        }
+
+        private void StartTransition(float scale) {
+            StartTransition(new List<float> { scale });
+        }
+
+        private void StartTransition(List<float> scales) {
+            if (transition != null) StopCoroutine(transition);
+
+            transition = StartCoroutine(Transition(scales, transitionTime));
+        }
+
+        private IEnumerator Transition(List<float> scales, float delay) {
+            float fractionalDelay = delay / scales.Count;
+
+            foreach (float scale in scales) {
+                float time = 0;
+                Vector3 vectorScale = originalScale * scale;
+
+                while (time <= fractionalDelay) {
+                    transform.localScale = Vector3.Lerp(transform.localScale, vectorScale, time / fractionalDelay);
+                    time += Time.deltaTime;
+                    yield return null;
+                }
+            }
+        }
+    }
+}
